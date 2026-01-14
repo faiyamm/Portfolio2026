c20ee942b6407c8e2d3b51de46bed1c26cb3bbef
faiyamm <sofia.almaraz@uabc.edu.mx>
2026-01-14 01:43:13 -0800

    Add project cards components

--- src/components/sections/Education.tsx
+++ src/components/sections/Education.tsx
@@ -22,17 +22,17 @@ export const Education = () => {
                 {educationData.map((education, index) => (
                     <div
                         key={index}
-                        className="p-8 border border-white/10 rounded-2xl bg-white/5 hover:bg-white/10 transition-colors flex flex-col gap-4"
+                        className="p-8 rounded-3xl bg-white/5 hover:bg-white/10 transition-colors flex flex-col gap-4 border border-white/5"
                     >
                         <div className="flex flex-col lg:flex-row justify-between gap-2">
                             <h3 className="text-xl font-bold text-white uppercase tracking-tight">
                                 {education.school}
                             </h3>
-                            <span className="text-white/60 text-sm md:text-base font-medium lg:text-right text-nowrap">
+                            <span className="text-neutral-200 text-xs font-medium lg:text-right">
                                 {education.period}
                             </span>
                         </div>
-                        <p className="text-white/80 leading-relaxed font-normal">
+                        <p className="text-neutral-300 leading-relaxed font-normal text-sm">
                             {education.description}
                         </p>
                     </div>
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,22 +1,9 @@
-
-export interface Project {
+export interface CardInfo {
     id: string;
     title: string;
     shortDescription: string;
     fullDescription: string;
     date: string;
     imageUrl: string;
-    techStack: string[];
-    liveDemoUrl?: string;
-    features: string[];
-}
-
-export interface WorkExperience {
-    id: string;
-    title: string;
-    company: string;
-    date: string;
-    shortDescription: string;
-    fullDescription: string;
-    imageUrl: string;
+    liveUrl?: string;
 }
